4415445fbbf8f1161f9a952f21c18db2917e8006
Muhammad Harris <harris.perceptron@gmail.com>
2025-05-04 06:51:40 +0500

    updated crow main

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,15 @@ int main()
   main2();
     crow::SimpleApp app;
 
-    CROW_ROUTE(app, "/")([](){
-        return "Hello world";
+ CROW_ROUTE(app, "/")
+    ([](){
+        return R"(
+            <html>
+                <head><title>Hello</title></head>
+                <body><h1>Hello, Crow!</h1></body>
+            </html>
+        )";
     });
 
-    app.port(18080).run();
+    app.port(8080).multithreaded().run();
 }
